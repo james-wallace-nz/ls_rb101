0ce13a642c33bc9ae090203c2da894734ffbb5f4
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-09-08 18:36:10 +1200

    Small Problems: Easy 4: 9 - 10, Easy 5: 1

--- small_problems/5_easy_4/10.rb
+++ small_problems/5_easy_4/10.rb
@@ -4,12 +4,48 @@
 
 # You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. You may, however, use integer_to_string from the previous exercise.
 
-def signed_integer_to_string(int)
+DIGITS_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+def integer_to_string(int)
+  result = ''
+  loop do
+    int, remainder = int.divmod(10)
+    result.prepend(DIGITS_ARRAY[remainder])
+    break if int.zero?
+  end
+  result
+end
+
+def signed_integer_to_string(int)
+  if int.negative?
+    "-#{integer_to_string(-int)}"
+  elsif int.positive?
+    "+#{integer_to_string(int)}"
+  else
+    "#{integer_to_string(int)}"
+  end
 end
 
 # Examples
 
-signed_integer_to_string(4321) == '+4321'
-signed_integer_to_string(-123) == '-123'
-signed_integer_to_string(0) == '0'
+puts signed_integer_to_string(4321) # == '+4321'
+puts signed_integer_to_string(-123) # == '-123'
+puts signed_integer_to_string(0) # == '0'
+
+# Refactor our solution to reduce the 3 integer_to_string calls to just one.
+
+def signed_integer_to_string(int)
+  sign = ''
+  if int.negative?
+    sign = '-'
+    number = -int
+  elsif int.positive?
+    sign = '+'
+    number = int
+  else
+    number = int
+  end
+  string = integer_to_string(number)
+  string.prepend(sign) if int != 0
+  string
+end
--- small_problems/5_easy_4/9.rb
+++ small_problems/5_easy_4/9.rb
@@ -4,12 +4,52 @@
 
 # You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. Your method should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
 
-def integer_to_string(int)
+DIGITS = {
+  0 => '0',
+  1 => '1',
+  2 => '2',
+  3 => '3',
+  4 => '4',
+  5 => '5',
+  6 => '6',
+  7 => '7',
+  8 => '8',
+  9 => '9'
+}
 
+def integer_to_string(int)
+  divisor = 1
+  digits = []
+  loop do
+    divisor *= 10
+    digit = int % divisor
+    digit /= (divisor / 10) if divisor > 10
+    digits << DIGITS[digit]
+    break if int % divisor == int
+  end
+  digits.reverse.join('')
 end
 
 # Examples
 
-integer_to_string(4321) == '4321'
-integer_to_string(0) == '0'
-integer_to_string(5000) == '5000'
+p integer_to_string(4321) # == '4321'
+p integer_to_string(0) # == '0'
+p integer_to_string(5000) # == '5000'
+
+DIGITS_ARRAY = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# Solution
+
+def integer_to_string(int)
+  result = ''
+  loop do
+    int, remainder = int.divmod(10)
+    result.prepend(DIGITS_ARRAY[remainder])
+    break if int == 0
+  end
+  result
+end
+
+p integer_to_string(4321) # == '4321'
+p integer_to_string(0) # == '0'
+p integer_to_string(5000) # == '5000'
