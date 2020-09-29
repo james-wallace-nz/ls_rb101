88392df9b9bbb509a17e869f8202cc0268700d1c
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-09-30 00:18:44 +1300

    Small Problems: Medium 2: 5 - 6

--- small_problems/12_medium_2/5.rb
+++ small_problems/12_medium_2/5.rb
@@ -7,9 +7,35 @@
 
 # Write a method that takes the lengths of the 3 sides of a triangle as arguments, and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending on whether the triangle is equilateral, isosceles, scalene, or invalid.
 
-def triangle(one, two, three)
+# inputs: 3 sides integers
 
+def triangle(side_one, side_two, side_three)
+  sides_sorted = [side_one, side_two, side_three].sort
+  return :invalid if side_one <= 0 || side_two <= 0 || side_three <= 0
+  return :invalid if sides_sorted[0] + sides_sorted[1] <= sides_sorted[2]
+  if sides_sorted[0] == sides_sorted[1] && sides_sorted[1] == sides_sorted[2]
+    return :equilateral
+  elsif (sides_sorted[0] == sides_sorted[1] && sides_sorted[1] != sides_sorted[2]) || (sides_sorted[1] == sides_sorted[2] && sides_sorted[0] != sides_sorted[1])
+    return :isosceles
+  else
+    return :scalene
+  end
 end
+
+def triangle(side_one, side_two, side_three)
+  sides_sorted = [side_one, side_two, side_three].sort
+  case
+  when sides_sorted.include?(0) || sides_sorted[0] + sides_sorted[1] <= sides_sorted[2]
+    :invalid
+  when side_one == side_two && side_two == side_three
+    :equilateral
+  when side_one == side_two || side_two == side_three || side_one == side_three
+    :isosceles
+  else
+    :scalene
+  end
+end
+
 
 # Examples:
 
