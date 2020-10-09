b07897176118c7a3a0f55d481d48f4214125083b
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-10-09 23:36:47 +1300

    Small Problems: Advanced 1: 8 - 9

--- small_problems/13_advanced_1/8.rb
+++ small_problems/13_advanced_1/8.rb
@@ -19,8 +19,6 @@
 # recursively break down into smallest nested arrays
 # return to next level up by recursively comparing each adjacent nested array - use merge from ex7
 
-
-
 def merge(array1, array2)
   index2 = 0
   result = []
@@ -63,27 +61,210 @@ puts '---'
 
 # Every recursive algorithm can be reworked as a non-recursive algorithm. Can you write a method that performs a non-recursive merge sort?
 
-# input: array
-# output: sorted array
+# input: array of elements
+# output: sorted array of elements
+# algorithm:
+# iterate through each pair of elements and merge them
+  # if last pair is one sided only take first element
 
-# break down into smallest nested arrays
-#   loop through each even element in array and split even + next odd into new array.
-#   loop middle times
 
-# return to next level up by comparing each adjacent nested array - use merge from ex7
+# def merge(new_array, left, mid, right)
+#   n1 = mid - left + 1
+#   n2 = right - mid
+#   left_arr = [] * n1
+#   right_arr = [] * n2
 
-def merge_sort(array)
-  result = array.map { |e| [e] }
+#   for i in 0..n1
+#     left_arr[i] = new_array[left + 1]
+#   end
 
-  result
+#   for i in 0..n2
+#     right_arr[i] = new_array[mid + i + 1]
+#   end
+
+#   p left_arr
+#   p right_arr
+#   p '---'
+
+#   i, j, k = 0, 0, left
+
+#   while i < n1 && j < n2
+#     if left_arr[i] > right_arr[j]
+#       new_array[k] = right_arr[j]
+#       j += 1
+#     else
+#       new_array[k] = left_arr[i]
+#       i += 1
+#     end
+#     k += 1
+#   end
+
+#   while i < n1
+#     new_array[k] = left_arr[i]
+#     i += 1
+#     k += 1
+#   end
+
+#   while j < n2
+#     new_array[k] = right_arr[j]
+#     j += 1
+#     k += 1
+#   end
+
+#   new_array
+# end
+
+# def merge_sort(array)
+#   new_array = array
+#   current_size = 1
+#   max_index = array.size - 1
+
+#   while current_size < max_index
+#     left = 0
+#     while left < max_index
+#       mid = [left + current_size - 1, max_index].min
+#       right = if 2 * current_size + left - 1 > max_index
+#                 max_index
+#               else
+#                 2 * current_size + left - 1
+#               end
+
+#       new_array = merge(new_array, left, mid, right)
+#       left += current_size * 2
+#     end
+#     current_size *= 2
+#   end
+#   new_array
+# end
+
+# a = [12, 11, 13, 5, 6, 7]
+# p merge_sort(a)
+
+def merge(new_array, temp, frm, mid, to)
+  k = frm
+  i = frm
+  j = mid + 1
+
+  puts "frm: #{frm}, mid: #{mid}, to: #{to}"
+  puts "k: #{k}, i: #{i},j: #{j}"
+
+  while i <= mid && j <= to
+    p "#{new_array[i]} : #{new_array[j]}"
+    if new_array[i] < new_array[j]
+      temp[k] = new_array[i]
+      i += 1
+    else
+      temp[k] = new_array[j]
+      j += 1
+    end
+    p temp
+    k += 1
+  end
+
+  while i < new_array.size && i <= mid
+    puts "mop up:  i: #{i}, k: #{k}, #{new_array.size}, #{mid}"
+    temp[k] = new_array[i]
+    k += 1
+    i += 1
+  end
+
+  p "temp: #{temp}"
+
+  (frm...(to + 1)).each do |l|
+    new_array[l] = temp[l]
+  end
+
+  new_array
 end
 
-p merge_sort([9, 5, 7, 1]) # == [1, 5, 7, 9]
+def merge_sort(array)
+  low = 0
+  high = array.size - 1                                                # 6
+  new_array = array.clone
+  temp = new_array.clone
 
-p merge_sort([5, 3]) # == [3, 5]
+  current_size = 1
+  while current_size <= high - low                                                # 1 <= (6 - 0)
+    puts "current size: #{current_size}\n\n"
+    (low...high).step(2 * current_size).each do |i|                               # i in (0...6).step(2)  0, 2, 4 then 0, 4 then 0
+      frm = i                                                                     # 0..5                  0, 2, 4 then 0, 4 then 0
+      mid = i + current_size - 1                                                  # 0 + 1 - 1 => 0, 2 + 1 - 1 => 2, 4 + 1 - 1 => 4  then 0 + 2 - 1 => 1, 4 + 2 - 1 => 5 0 + 4 - 1 => 3
+      to = [i + 2 * current_size - 1, high].min                                   # [0 + 2 * 1 - 1, 6].min = [1, 6].min => 1
+      new_array = merge(new_array, temp, frm, mid, to)
+      p new_array
+      puts "\n\n"
+    end
+    current_size *= 2                                                             # 1, 2, 4, 6
+  end
+  new_array
+end
 
-p merge_sort([6, 2, 7, 1, 4]) # == [1, 2, 4, 6, 7]
+a = [5, 7, -9, 3, -4, 2, 8]
+p merge_sort(a)
+# p a
+# https://www.techiedelight.com/iterative-merge-sort-algorithm-bottom-up/
 
-p merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) # == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+# p merge_sort([9, 5, 7, 1]) == [1, 5, 7, 9]
+# [9, 5, 7, 1]
+# [[9, 5],[7, 1]]
 
-p merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) # == [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
+# p merge_sort([5, 3]) == [3, 5]
+
+# p merge_sort([6, 2, 7, 1, 4]) == [1, 2, 4, 6, 7]
+
+# merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) # == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+
+# 0
+# array
+# ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie']                                    split[0..6]
+
+# 1
+# array.each
+# [['Sue', 'Pete', 'Alice'], ['Tyler', 'Rachel', 'Kim', 'Bonnie']
+
+# [
+  # ['Sue', 'Pete', 'Alice'],                                                                     split[0]
+  # ['Tyler', 'Rachel', 'Kim', 'Bonnie']                                                          split[1]
+# ]
+
+# 2
+# array.each.each
+# [[['Sue'], ['Pete', 'Alice']], [['Tyler', 'Rachel'], ['Kim', 'Bonnie']]
+
+# [
+#   [                                                                                             spilt[0]
+#     ['Sue']                                                                                     [0]
+#     ['Pete', 'Alice'],                                                                          [1]
+#   ],
+#   [                                                                                             spilt[1]
+#     ['Tyler', 'Rachel'],                                                                        [0]
+#     ['Kim', 'Bonnie']                                                                           [1]
+#   ]
+# ]
+
+# 3
+# [[[['Sue']], [['Pete'], ['Alice']]], [[['Tyler'], ['Rachel']], [['Kim'], ['Bonnie']]]
+
+# [
+#   [                                                                                             spilt[0]
+#     [                                                                                                   [0]
+#       ['Sue']                                                                                              [0]
+#     ],
+#     [                                                                                                   [1]
+#       ['Pete'],                                                                                            [0]
+#       ['Alice']                                                                                            [1]
+#     ]
+#   ],
+#   [                                                                                             spilt[1]
+#     [                                                                                                   [0]
+#       ['Tyler'],                                                                                            [0]
+#       ['Rachel']                                                                                            [1]
+#     ],
+#     [                                                                                                   [1]
+#       ['Kim'],                                                                                              [0]
+#       ['Bonnie']                                                                                            [1]
+#     ]
+#   ]
+# ]
+
+# p merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) == [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
--- small_problems/13_advanced_1/9.rb
+++ small_problems/13_advanced_1/9.rb
@@ -16,21 +16,115 @@
 
 # Write two methods: one that takes a Rational number as an argument, and returns an Array of the denominators that are part of an Egyptian Fraction representation of the number, and another that takes an Array of numbers in the same format, and calculates the resulting Rational number. You will need to use the Rational class provided by Ruby.
 
+# The Rational class represents fractions
+# Rational(a, b) is a rational object a / b
+# a / 1 = integer, 2 / 1 = 2
+# Array is the denominators, numerator is always 1
+# can have 1 / 1 as unit fraction
+
+# input: rational number
+# output: array of denominators that make up the egyptian fraction represenation of the number
+# algorithm:
+# 1a. find largest fraction: denominator / numerator rounded up = denominator (n) of largest fraction 1 / n
+  # 1b. pass unit fraction denominator to result array
+# 2. subtract unit fraction from rational number to get remainder
+# repeat 1 on remainder until remainder is a unit fraction itself
+# 3. return result array of denominators
+
+# unit fraction denominator cannot be repeated
+
+# => split numerator and denominator into variables?
+
+def egyptian(fraction)
+  egyption_denominators = []
+  remainder = fraction
+  numerator_remainder = remainder.numerator
+  denominator_remainder = remainder.denominator
+
+  loop do
+    largest_denominator = (denominator_remainder / numerator_remainder.to_f).ceil
+    largest_denominator += 1 if egyption_denominators.include?(largest_denominator) # error here - not always + 1
+    egyption_denominators << largest_denominator
+
+    remainder = Rational(numerator_remainder * largest_denominator, denominator_remainder * largest_denominator) -
+                Rational(1 * denominator_remainder, largest_denominator * denominator_remainder)
+
+    break if egyption_denominators.reduce(0) { |memo, denom| memo + Rational(1, denom) } == fraction
+
+    numerator_remainder = remainder.numerator
+    denominator_remainder = remainder.denominator
+  end
+  egyption_denominators
+end
+
+# input: array of egyptian fractions
+# output: resulting rational number
+# algorithm:
+
+# def unegyptian(egyption_fractions)
+
+# end
+
 # Examples:
 
-egyptian(Rational(2, 1))    # -> [1, 2, 3, 6]
-egyptian(Rational(137, 60)) # -> [1, 2, 3, 4, 5]
-egyptian(Rational(3, 1))    # -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 230, 57960]
+p egyptian(Rational(2, 1)) == [1, 2, 3, 6]
 
-unegyptian(egyptian(Rational(1, 2))) == Rational(1, 2)
-unegyptian(egyptian(Rational(3, 4))) == Rational(3, 4)
-unegyptian(egyptian(Rational(39, 20))) == Rational(39, 20)
-unegyptian(egyptian(Rational(127, 130))) == Rational(127, 130)
-unegyptian(egyptian(Rational(5, 7))) == Rational(5, 7)
-unegyptian(egyptian(Rational(1, 1))) == Rational(1, 1)
-unegyptian(egyptian(Rational(2, 1))) == Rational(2, 1)
-unegyptian(egyptian(Rational(3, 1))) == Rational(3, 1)
+p egyptian(Rational(137, 60)) == [1, 2, 3, 4, 5]
+
+p egyptian(Rational(3, 1)) # == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 230, 57960]
+
+# unegyptian(egyptian(Rational(1, 2))) == Rational(1, 2)
+# unegyptian(egyptian(Rational(3, 4))) == Rational(3, 4)
+# unegyptian(egyptian(Rational(39, 20))) == Rational(39, 20)
+# unegyptian(egyptian(Rational(127, 130))) == Rational(127, 130)
+# unegyptian(egyptian(Rational(5, 7))) == Rational(5, 7)
+# unegyptian(egyptian(Rational(1, 1))) == Rational(1, 1)
+# unegyptian(egyptian(Rational(2, 1))) == Rational(2, 1)
+# unegyptian(egyptian(Rational(3, 1))) == Rational(3, 1)
 
 # Every rational number can be expressed as an Egyptian Fraction. In fact, every rational number can be expressed as an Egyptian Fraction in an infinite number of different ways. Thus, the first group of examples may not show the same values as your solution. They do, however, show the expected form of the solution. The remaining examples merely demonstrate that the output of egyptian can be reversed by unegyptian.
 
 # For more info on Egyptian Fractions, see this page.
+
+# http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fractions/egyptian.html#calc1
+
+# t / b < 1
+# if t = 1 then it is solved because it is already a unit fraction
+
+#   Find the biggest unit fraction we can and take it from t / b
+#   Repeat with what is left.
+
+# examples:
+
+# 521 / 1050
+# 521 is < 1 / 2 1050 but bigger than 1 / 3 1050 => largest fraction we can take away is 1 / 3
+
+# 521 / 1050 = 1 / 3 + R
+# 521 / 1050 - 1 / 3 = 171 / 1050 = 57 / 350
+
+# repeat the process on 57 / 350
+
+# The largest unit fraction is 1 / 7 and the remainder is 1 / 50 (7 / 350)
+
+# divide the bottom number (350) by the top one (57) and get 6.14. We need a number larger than 6 since we have 6 + 0.14. The next one above 6 is 7.
+
+# The numerator and denominator keep reducign until the numerator reaches 1 then the process stops = the final remainder is a unit fraction so we can stop.
+
+# example
+
+# 5 / 21
+
+# puts 21.to_f / 5
+
+# 5 / 21 = 1 / 5 + R
+# 5 / 21 - 1 / 5 = 25 / 105 - 21 / 105 = 4 / 105
+
+# puts 105.to_f / 4 = 1 / 27
+
+# 5 / 21 = 1 / 5 - 1 / 27 + R
+
+# 4 / 105 - 1 / 27 = 108 / 2835 - 105 / 2835 = 3 / 2835 = 1 / 945
+
+# 5 / 21 = 1 / 5 - 1 / 27 + 1 / 945
+
+p egyptian(Rational(5, 21)) == [5, 27, 945]
