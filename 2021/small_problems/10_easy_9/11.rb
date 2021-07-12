c3e41c860754eaac086ee9fbff0934206c9b7b02
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-07-13 08:11:31 +1200

    small problems easy 9 8 & 11

--- 2021/small_problems/10_easy_9/11.rb
+++ 2021/small_problems/10_easy_9/11.rb
@@ -1,8 +1,29 @@
 # Given the array...
 
+# input: array of strings
+# output: print array of anagrams - words with the same letters in different orders
+# algo:
+# 1. create sorted_words hash - each word in words is sorted by character. sorted_word is the key with empty array as value
+# 2. iterate through words and add element to sorted_words hash value array
+# 3. iterate through nested arrays and print
+
+def anagrams(words)
+  anagrams = Hash.new
+  sorted_words = words.map { |word| word.chars.sort.join }.uniq
+  sorted_words.each { |sorted_word| anagrams[sorted_word] = [] }
+  words.each do |word|
+    anagrams[word.chars.sort.join] << word
+  end
+  anagrams.each_value do |value|
+    p value
+  end
+end
+
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
+
+anagrams(words)
 
 # Write a program that prints out groups of words that are anagrams. Anagrams are words that have the same exact letters in them but in a different order. Your output should look something like this:
 
--- 2021/small_problems/10_easy_9/8.rb
+++ 2021/small_problems/10_easy_9/8.rb
@@ -2,12 +2,23 @@
 
 # You may assume that the count argument will always have a value of 0 or greater, while the starting number can be any integer value. If the count is 0, an empty list should be returned.
 
-def sequence(first, second)
+# input: count and starting integer
+# output: array of numbers from starting integer increasing by count index multiple
 
+def sequence(count, initial)
+  result = []
+  count.times do |index|
+    result << initial * (index + 1)
+  end
+  result
+end
+
+def sequence(count, initial)
+  (1..count).map { |value| initial * value }
 end
 
 # Examples:
-sequence(5, 1) == [1, 2, 3, 4, 5]
-sequence(4, -7) == [-7, -14, -21, -28]
-sequence(3, 0) == [0, 0, 0]
-sequence(0, 1000000) == []
+puts sequence(5, 1) == [1, 2, 3, 4, 5]
+puts sequence(4, -7) == [-7, -14, -21, -28]
+puts sequence(3, 0) == [0, 0, 0]
+puts sequence(0, 1000000) == []
