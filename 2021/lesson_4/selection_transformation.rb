380a17b3fb722a25b0198fd4062b8234e89a983b
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-08-01 14:52:00 +1200

    lesson 4 selection & transformation

--- 2021/lesson_4/selection_transformation.rb
+++ 2021/lesson_4/selection_transformation.rb
@@ -76,3 +76,52 @@ end
 
 p double_odd_indices(my_numbers)  # => [2, 4, 6, 14, 2, 6]
 p my_numbers
+
+
+puts '------'
+
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+
+def multiply(array, multiplier)
+  multiplied = []
+  count = 0
+
+  loop do
+    break if count == array.size
+    multiplied << array[count] * multiplier
+    count += 1
+  end
+
+  multiplied
+end
+
+p multiply(my_numbers, 3)
+
+
+puts '-------'
+
+
+question = 'How many times does a particular character appear in this sentence?'
+
+def select_letter(string, letter)
+  selected_letters = ''
+  count = 0
+
+  loop do
+    break if count == string.size
+    selected_letters << string[count] if string[count] == letter
+    count += 1
+  end
+
+  selected_letters
+end
+
+puts select_letter(question, 'a') == "aaaaaaaa"
+puts select_letter(question, 't') == "ttttt"
+puts select_letter(question, 'z') == ""
+
+puts select_letter(question, 'a').size == 8
+puts select_letter(question, 't').size == 5
+puts select_letter(question, 'z').size == 0
+
