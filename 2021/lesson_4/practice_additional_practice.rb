250246ddc8ffd2c6380d65e64e064657d5c7fde3
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-08-02 16:34:27 +1200

    lesson 4 practice problems #2

--- 2021/lesson_4/practice_additional_practice.rb
+++ 2021/lesson_4/practice_additional_practice.rb
@@ -6,6 +6,13 @@ flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 
 # Turn this array into a hash where the names are the keys and the values are the positions in the array.
 
+flintstones_hash = {}
+
+flintstones.each_with_index do |value, index|
+  flintstones_hash[value] = index
+end
+
+# p flintstones_hash
 
 
 # 2.
@@ -14,6 +21,21 @@ flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 
+total = 0
+ages.each do |_, age|
+  total += age
+end
+# puts total
+
+total = ages.reduce(0) do |sum, (_, age)|
+  sum + age
+end
+
+# puts total
+
+# puts ages.values.reduce(&:+)
+
+# puts ages.values.sum
 
 
 # 3.
@@ -24,6 +46,14 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 
 # remove people with age 100 and greater.
 
+ages.delete_if { |_, age| age >= 100 }
+# p ages
+
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+ages.keep_if { |_, age| age < 100 }
+# p ages
 
 
 # 4.
@@ -31,6 +61,24 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 # Pick out the minimum age from our current Munster family hash:
 
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+# minimum = nil
+# ages.each do |_, age|
+#   minimum ||= age
+#   if age < minimum
+#     minimum = age
+#   end
+# end
+
+# puts minimum
+
+minimum = ages.values.reduce do |age, min|
+  age < min ? age : min
+end
+
+# p minimum
+
+# puts ages.values.min
 
 
 # 5.
@@ -41,12 +89,19 @@ flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
 # Find the index of the first name that starts with "Be"
 
+# puts flintstones.find_index { |name| name[0..1] == 'Be' }
+
+# puts flintstones.index { |name| name[0, 2] == 'Be' }
+
 
 # 6.
 
 # Amend this array so that the names are all shortened to just the first three characters:
 
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.map! { |name| name[0, 3] }
+# p flintstones
 
 
 # 7.
@@ -59,26 +114,60 @@ statement = "The Flintstones Rock"
 
 # { "F"=>1, "R"=>1, "T"=>1, "c"=>1, "e"=>2, ... }
 
+frequency = Hash.new(0)
+statement.chars.each do |char|
+  frequency[char] += 1
+end
+# p frequency
+
+result = {}
+letters = ('A'..'Z').to_a + ('a'..'z').to_a
+letters.each do |letter|
+  frequency = statement.scan(letter).count
+  result[letter] = frequency if frequency > 0
+end
+# p result
+
 
 # 8.
 
 # What happens when we modify an array while we are iterating over it? What would be output by this code?
 
-numbers = [1, 2, 3, 4]
+# numbers = [1, 2, 3, 4]
 
-numbers.each do |number|
-  p number
-  numbers.shift(1)
-end
+# numbers.each do |number|
+#   p number
+#   numbers.shift(1)
+# end
+
+# outputs `1`
+# `numbers.shift(1)` returns `[1]`
+
+# array is now `[2, 3, 4]`
+# outputs `3`
+# `numbers.shift(1)` returns `[2]`
+
+# array is now `[3, 4]`
+# `each` returns `[3, 4]`
+
 
 # What would be output by this code?
 
-numbers = [1, 2, 3, 4]
+# numbers = [1, 2, 3, 4]
 
-numbers.each do |number|
-  p number
-  numbers.pop(1)
-end
+# numbers.each do |number|
+#   p number
+#   numbers.pop(1)
+# end
+
+# outputs `1`
+# `numbers.pop(1)` destructively removes last element `4` and returns `4`
+# array is now [1, 2, 3]
+
+# outputs `2`
+# `numbers.pop(1)` destructively removes last element `3` and returns `3`
+# array is now [1, 2]/
+# each stops iterating and returns [1, 2]
 
 
 # 9.
@@ -89,10 +178,24 @@ words = "the flintstones rock"
 
 # would be:
 
-words = "The Flintstones Rock"
+# words = "The Flintstones Rock"
 
 # Write your own version of the rails titleize implementation.
 
+
+# p words.split.map { |word| word.capitalize }.join(' ')
+
+
+def titleize(string)
+  words = string.split
+  words.map! do |word|
+    word.capitalize
+  end
+  words.join(' ')
+end
+
+title = titleize(words)
+# p title
 
 
 # 10.
@@ -109,10 +212,21 @@ munsters = {
 
 # Modify the hash such that each member of the Munster family has an additional "age_group" key that has one of three values describing the age group the family member is in (kid, adult, or senior). Your solution should produce the hash below
 
-{ "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
+updated = { "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
   "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
   "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
   "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
   "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
 
 # Note: a kid is in the age range 0 - 17, an adult is in the range 18 - 64 and a senior is aged 65+.
+
+munsters.each do |_, details|
+  age_group = case details["age"]
+              when  (0..17) then 'kid'
+              when (18..64) then 'adult'
+              else 'senior'
+              end
+  details['age_group'] = age_group
+end
+
+puts munsters == updated
