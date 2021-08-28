6f6520be68f014fb4f44a62e275db8d1329d00b0
James Wallace <4538317+james-wallace-nz@users.noreply.github.com>
2021-08-28 23:03:19 +1200

    Small problems Advanced 5, 7

--- 2021/small_problems/13_advanced_1/5.rb
+++ 2021/small_problems/13_advanced_1/5.rb
@@ -31,26 +31,103 @@
 
 # Write a method that takes an arbitrary matrix and rotates it 90 degrees clockwise as shown above.
 
+=begin
+
+PROBLEM
+
+Rotate a nested array (matrix) 90 degrees clockwise
+
+Input: matrix
+Output: rotated matrix
+
+
+DATA STRUCTURE
+
+Nested array
+
+
+ALGORITHM
+
+create new empty array
+Iterate across columns (0 to 2)
+  create new array for row
+  Iterate up rows (2 to 0)
+    add value to new row
+  add new row array to new array
+
+
+row, col
+
+0,0     0,2
+0,1     1,2
+0,2     2,2
+
+1,0     0,1
+1,1     1,1
+1,2     2,1
+
+2,0     0,0
+2,1     1,0
+2,2     2,0
+
+
+CODE
+=end
+
+def rotate90(matrix)
+  rotated = Array.new
+
+  (0...matrix.first.size).each do |column_index|
+    new_row = []
+    (matrix.size - 1).downto(0) do |row_index|
+      new_row << matrix[row_index][column_index]
+    end
+
+    # new_row = (matrix.size - 1).downto(0).map do |row_index|
+    #             matrix[row_index][column_index]
+    #           end
+
+    rotated << new_row
+  end
+
+  rotated
+end
+
+
 # Examples
 
-# matrix1 = [
-#   [1, 5, 8],
-#   [4, 7, 2],
-#   [3, 9, 6]
+matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+# [
+#   [3, 4, 1],
+#   [9, 7, 5],
+#   [6, 2, 8]
 # ]
 
-# matrix2 = [
-#   [3, 7, 4, 2],
-#   [5, 1, 0, 8]
-# ]
 
-# new_matrix1 = rotate90(matrix1)
-# new_matrix2 = rotate90(matrix2)
-# new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8]
+]
 
-# p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
-# p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
-# p new_matrix3 == matrix2
+new_matrix1 = rotate90(matrix1)
+new_matrix2 = rotate90(matrix2)
+new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+
+p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+p new_matrix3 == matrix2
 
 # This program should print "true" three times.
 
+=begin
+Bonus: rotations based on argument:
+90
+180
+270
+360
+=end
--- 2021/small_problems/13_advanced_1/7.rb
+++ 2021/small_problems/13_advanced_1/7.rb
@@ -4,9 +4,78 @@
 
 # Your solution should not mutate the input arrays.
 
+=begin
+PROBLEM
+
+Merge two sorted arrays without using the #sort method
+Return a new array
+
+
+Input: two arrays
+Output: new merged array
+
+
+DATA STRUCTURE
+
+Two Arrays
+
+
+ALGORITHM
+
+- create variable for new merged array
+- create counter variables for each array argument
+- create a loop that iterates
+  - compare current element at counter for each element
+  - add smaller element to new merged array
+  - increment counter for array when element from that array is added to new merged array
+  break from loop once counter for both is equal to or greater than array size
+- return new array
+
+CODE
+=end
+
+def merge(array_one, array_two)
+  merged = Array.new
+  count_one, count_two = 0, 0
+
+  loop do
+    current_one = array_one[count_one]
+    current_two = array_two[count_two]
+
+    if current_two.nil? || (!current_one.nil? && current_one <= current_two)
+      merged << current_one
+      count_one += 1
+    elsif current_one.nil? || (!current_two.nil? && current_two < current_one)
+      merged << current_two
+      count_two += 1
+    end
+
+    break if count_one >= array_one.size && count_two >= array_two.size
+  end
+
+  merged
+end
+
+
+def merge(array_one, array_two)
+  merged = Array.new
+  index_two = 0
+
+  array_one.each do |value_one|
+    while index_two < array_two.size && array_two[index_two] < value_one
+      merged << array_two[index_two]
+      index_two += 1
+    end
+    merged << value_one
+  end
+
+  merged.concat(array_two[index_two..-1])
+end
+
+
 # Examples:
 
-# merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
-# merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
-# merge([], [1, 4, 5]) == [1, 4, 5]
-# merge([1, 4, 5], []) == [1, 4, 5]
+p merge([1, 5, 9], [2, 6, 8]) # == [1, 2, 5, 6, 8, 9]
+p merge([1, 1, 3], [2, 2]) # == [1, 1, 2, 2, 3]
+p merge([], [1, 4, 5]) # == [1, 4, 5]
+p merge([1, 4, 5], []) # == [1, 4, 5]
