78866cf3b18524276b391bd5c29d4cdd9e278783
James Wallace <4538317+NZLWally@users.noreply.github.com>
2024-07-03 22:21:37 +1200

    Add exercise files

--- 2022/small_problems/5_easy_4/2.rb
+++ 2022/small_problems/5_easy_4/2.rb
@@ -2,25 +2,66 @@
 
 # New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 20th century.
 
-# input: integer
+# input: integer (year)
 # outputs: string, century ending in st, nd, rd or th
 # algo:
+# 1. determine century
+# 2. determine suffix
+# 3. concatenate and return string
 
+# century
+# remainder after dividing by 100?
 
+# endings:
+# 1         'st'
+# 2         'nd'
+# 3         'rd'
+# 4 - 10    'th'
+# 11 - 13   'th'
 
 def century(year)
+  century = year / 100 + (year % 100 > 0 ? 1 : 0)
+  "#{century}#{suffix(century)}"
+end
 
-
+def suffix(century)
+  if [11, 12, 13].include?(century % 100)
+    'th'
+  else
+    case century % 10
+    when 0 then 'th'
+    when 1 then 'st'
+    when 2 then 'nd'
+    when 3 then 'rd'
+    when 4..9 then 'th'
+    end
+  end
 end
 
 # Examples:
 
-century(2000) == '20th'
-century(2001) == '21st'
-century(1965) == '20th'
-century(256) == '3rd'
-century(5) == '1st'
-century(10103) == '102nd'
-century(1052) == '11th'
-century(1127) == '12th'
-century(11201) == '113th'
+puts century(2000) == '20th'
+
+
+puts century(2001) == '21st'
+
+
+puts century(1965) == '20th'
+
+
+puts century(256) == '3rd'
+
+
+puts century(5) == '1st'
+
+
+puts century(10103) == '102nd'
+
+
+puts century(1052) == '11th'
+
+
+puts century(1127) == '12th'
+
+
+puts century(11201) == '113th'
